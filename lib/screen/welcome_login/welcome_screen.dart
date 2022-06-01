069c24e21bca202c7ac0358b0851cc07d4fb3ddb
chetanshivaji <mchetan.more@gmail.com>
2022-06-01 12:03:19 +0530

    beautify remvoed stretch

--- lib/screen/welcome_login/welcome_screen.dart
+++ lib/screen/welcome_login/welcome_screen.dart
@@ -21,7 +21,6 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Center(
               child: Text(
